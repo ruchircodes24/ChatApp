506e11e10b804b1ac120e404950320eb537e3421
ruchircodes24 <gavshinderuchir@gmail.com>
2024-02-11 23:57:48 +0530

    Updated Welcome component - added text to it

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Welcome() {
+  return (
+    <>
+        <div className='flex flex-col justify-center items-center'>
+            <h1 className='text-3xl font-bold'>Hey there! Welcome to ChatChit</h1>
+            <p>Sign In to interact with your peers</p>
+            <button>Sign In</button>
+        </div>
+    </>
+  )
+}
+
+export default Welcome
