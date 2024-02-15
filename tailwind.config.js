3bdd07bea972bb682d034d4681ba52c0c7795e3d
ruchircodes24 <gavshinderuchir@gmail.com>
2024-02-15 23:51:09 +0530

    created a custom font-family to apply google font in the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'kanit': ['kanit', 'arial']
+      }
+    },
   },
   plugins: [],
 }
