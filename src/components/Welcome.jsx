8dd930576358b535faf85f71e75cb7fff5cdfccf
ruchircodes24 <gavshinderuchir@gmail.com>
2024-02-15 23:49:56 +0530

    added styling to the page

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Welcome() {
   return (
     <>
-        <div className='flex flex-col justify-center items-center'>
+        <div className='flex flex-col justify-center h-screen bg-gray-700'>
             <h1 className='text-3xl font-bold'>Hey there! Welcome to ChatChit</h1>
             <p>Sign In to interact with your peers</p>
             <button>Sign In</button>
