064f31aef8fb5b213a5229b9a40a8e93e6cd2373
ruchircodes24 <gavshinderuchir@gmail.com>
2024-02-17 15:53:57 +0530

    added Sign in functionality to the Sign in button

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import {auth} from '../firebase'
+import { GoogleAuthProvider, signInWithRedirect } from 'firebase/auth'
 
 function Welcome() {
+  const googleSignIn = () => {
+    const provider = new GoogleAuthProvider()
+    signInWithRedirect(auth, provider)
+  }
   return (
     <>
       <div className='flex flex-grow flex-col justify-center items-center h-screen bg-black text-white'>
@@ -8,8 +14,9 @@ function Welcome() {
           <h1 className='text-3xl font-bold text-white t '>Hey <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-purple-400'>there!</span><br /> Welcome to <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-purple-400'>ChatChit</span></h1>
         </div>
         <div className='flex justify-center py-2 flex-col items-center w-fit'>
-          <p className='py-3 text-2xl'>Sign In to interact with your peers</p>
-          <button className='bg-transparent bg-gradient-to-r from-purple-600 to-purple-400  py-3 w-full rounded-2xl font-bold text-xl'>Sign In</button>
+          <p className='py-3 text-xl'>Sign In to interact with your peers</p>
+          <button className='bg-transparent bg-gradient-to-r from-purple-600 to-purple-400  py-3 w-full rounded-2xl font-bold text-xl'
+          onClick={googleSignIn}>Sign In</button>
         </div>
       </div>
     </>
